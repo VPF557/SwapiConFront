7491d65d642d25e69f21c231d22fd8bed8f50a86
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-29 18:10:03 +0200

    Añadidas dependencias al POM

---
+++ Backend/src/main/java/SwapiConFront/example/SwapiConFront/SwapiConFrontApplication.java
@@ -0,0 +1,13 @@
+package SwapiConFront.example.SwapiConFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiConFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiConFrontApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/SwapiConFront/example/SwapiConFront/SwapiConFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package SwapiConFront.example.SwapiConFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiConFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
