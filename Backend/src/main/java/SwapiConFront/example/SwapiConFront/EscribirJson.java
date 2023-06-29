c759c42f8bb1d9e0b48dbb1aa98e79f30fada2a6
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-29 18:19:19 +0200

    Clase escritura json refactorizada

---
+++ Backend/src/main/java/SwapiConFront/example/SwapiConFront/EscribirJson.java
@@ -0,0 +1,44 @@
+package SwapiConFront.example.SwapiConFront;
+
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class EscribirJson {
+    static void escribirJsonPlanet(ArrayList<Planet> listaPlanets)
+    {
+
+        Gson gson = new Gson();
+
+        try (FileWriter writer = new FileWriter("Planets.json")) {
+            gson.toJson(listaPlanets, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void escribirJsonPeticiones(ArrayList<Param> listaParams)
+    {
+        Gson gson = new Gson();
+
+        try (FileWriter writer = new FileWriter("Peticiones.json")) {
+            gson.toJson(listaParams, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void escribirJsonStarship(ArrayList<Starship> listaStarship)
+    {
+
+        Gson gson = new Gson();
+
+        try (FileWriter writer = new FileWriter("Starships.json")) {
+            gson.toJson(listaStarship, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
